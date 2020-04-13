5f45c2f4a781b826c6089adc1ac6f24794966ec9
Chris Hayes <chrisroberthayes@gmail.com>
2020-04-13 16:08:44 -0400

    Add sequelize

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from '../models/user';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: 'password',
+      database: 'database_development',
+      models: [User]
+    }),
+    SequelizeModule.forFeature([User])
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from '../models/user';
 
 @Injectable()
 export class AppService {
-  getData(): string {
-    return 'Hello world';
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User
+  ) {}
+
+  async getData(): Promise<User[]> {
+    return await this.userModel.findAll();
   }
 }
